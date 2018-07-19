c4a14660697f7677964b0e5291e32afeee12830d
Vygandas Pliasas <vygandas.pliasas@gmail.com>
2018-07-19 15:32:33 +0300

    Added component css; added some comments

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,9 @@
 import * as React from "react";
+
+// This is the main component that you need.
 import SlidingContainer from "react-sliding-container";
+// Component comes without CSS styles, so if you want to have default ones you must include this
+import "../node_modules/react-sliding-container/lib/SlidingContainer.css";
 
 import "assets/scss/App.scss";
 
@@ -8,8 +12,8 @@ export default class App extends React.Component<undefined, undefined> {
         return (
             <div className="container app-component">
                 <div className="row">
-                    <div className="col-12">
-                        <h1>Hello</h1>
+                    <div className="col-12 text-center py-5">
+                        <h1 className="display-5 mb-5">React Sliding Container Demo App</h1>
                         <SlidingContainer/>
                     </div>
                 </div>
